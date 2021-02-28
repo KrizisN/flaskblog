92d3cee17f3508dc1a21d2e5b0aa014a7e9e9677
nkrizskiy <nikita.fyvfyv@gmail.com>
2021-02-28 17:12:47 +0200

    Add view for update, delete and viewing posts

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,8 +20,14 @@ class Article(db.Model):
         return f"Article {self.id}"
 
 
-@app.route("/", methods=["POST", "GET"])
-def main_view():
+@app.route("/")
+def home_view():
+    posts = Article.query.order_by(Article.date).all()
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/add-post", methods=["POST", "GET"])
+def add_post():
     if request.method == "POST":
         title = request.form["title"]
         intro = request.form["intro"]
@@ -31,11 +36,46 @@ def main_view():
         try:
             db.session.add(article)
             db.session.commit()
+            return redirect("/")
         except:
             return "Ошибка"
-        return render_template("about.html")
-    else:
         return render_template("home.html")
+    else:
+        return render_template("add_post.html")
+
+
+@app.route("/post/<int:id>")
+def detail_post(id):
+    post = Article.query.get(id)
+    return render_template("detail_post.html", post=post)
+
+
+@app.route("/post/<int:id>/del")
+def delete_post(id):
+    post = Article.query.get_or_404(id)
+    try:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Ошибка"
+    return render_template("home.html", post=post)
+
+
+@app.route("/post/<int:id>/update", methods=["POST", "GET"])
+def update_post(id):
+    post = Article.query.get(id)
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.intro = request.form["intro"]
+        post.text = request.form["text"]
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка"
+    else:
+        return render_template("update_post.html", post=post)
 
 
 if __name__ == "__main__":
